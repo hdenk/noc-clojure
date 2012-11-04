258f768b1414fa3b3da227ab90f8d4db1c7e33e1
hdenk <helmut.denk@gmx.de>
2012-11-04 14:51:26 +0100

    rename move to check-edges and do some refactorings

--- code/vectors/bouncingball_vectors.clj
+++ code/vectors/bouncingball_vectors.clj
@@ -15,6 +15,7 @@
    :frame-rate 30
    :ball-x 100
    :ball-y 100
+   :ball-r 16
    :speed-x 2.5
    :speed-y 5})
 
@@ -28,11 +29,7 @@
   (q/background (params :background))
   (q/smooth))
 
-(defn render [ball]
-  (q/no-stroke)
-  (q/fill 255 10)
-  (q/rect 0 0 (q/width) (q/height))
-
+(defn- check-edges [ball]
   (let [location (:location @ball)]
     (if (or 
           (> (.-x location) (q/width)) 
@@ -41,7 +38,7 @@
         ball 
         update-in 
         [:velocity] 
-        (fn [v-old] (processing.core.PVector. (* (.-x v-old) -1) (.-y v-old)))))
+        #(processing.core.PVector. (* (.-x %) -1) (.-y %))))
 
     (if (or 
           (> (.-y location) (q/height)) 
@@ -50,19 +47,27 @@
         ball 
         update-in 
         [:velocity] 
-        (fn [v-old] (processing.core.PVector. (.-x v-old) (* (.-y v-old) -1))))))
+        #(processing.core.PVector. (.-x %) (* (.-y %) -1))))))
 
+(defn- move [ball]
   (let [velocity (:velocity @ball)]
     (swap! 
       ball 
       update-in 
       [:location] 
-      #(processing.core.PVector/add %1 %2) velocity))
+      #(processing.core.PVector/add %1 %2) velocity)))
 
-  ; Display circle at x location
+(defn render [ball]
+  (q/no-stroke)
+  (q/fill 255 10)
+  (q/rect 0 0 (q/width) (q/height))
+   
+  (check-edges ball)
+  (move ball)
+  ; Display circle at ball location
   (q/stroke 0)
   (q/fill 175)
-  (q/ellipse (.-x (:location @ball)) (.-y (:location @ball)) 16 16))
+  (q/ellipse (.-x (:location @ball)) (.-y (:location @ball)) (params :ball-r) (params :ball-r)))
 
 (defn gen-draw-fn [] 
   "gen function that renders the output"
