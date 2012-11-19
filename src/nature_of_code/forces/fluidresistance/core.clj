f2714a4747984b76f9c642802b79af10d2182b3c
hdenk <helmut.denk@gmx.de>
2012-11-19 04:12:47 +0100

    erste grundlegendes refactoring -> use protocols for mover and fluid

--- src/nature_of_code/forces/fluidresistance/core.clj
+++ src/nature_of_code/forces/fluidresistance/core.clj
@@ -2,10 +2,8 @@
   "Demonstration of multiple force acting on bodies (Mover class)
    Bodies experience gravity continuously
    Bodies experience fluid resistance when in water"
-  (:require [quil.core :as q]
-            [nature-of-code.forces.fluidresistance.scene :as scene]
-            [nature-of-code.forces.fluidresistance.mover :as mover])
-  (:import [processing.core PVector]))
+   (:require [quil.core :as q])
+   (:import [processing.core PVector]))
 
 (defmacro dbg
   "print debug-infos to console"
@@ -14,70 +12,147 @@
      [x# ~x] 
      (println "dbg:" '~x "=" x#) x#)) 
 
+;;;
+;;; Mover
+;;;
+
+(defprotocol Stateful
+  (next-state [this] "calc next state for the stateful object"))
+
+(defprotocol Massiv
+  (apply-force [this force] "apply force to the masive object"))
+
+(defprotocol Movable
+  (check-edges [this] "check if the drawable object is out ouf bounds and react"))
+
+(defprotocol Drawable
+  (draw [this] "draw the drawable object to an output-device"))
+
+(defrecord Mover [id mass location velocity acceleration color]
+  Stateful  
+  (next-state [this]
+    (let [next-location (PVector/add location velocity)
+          next-velocity (PVector/add velocity acceleration)
+          next-acceleration (PVector/mult acceleration (float 0))]
+      (assoc this :location next-location :velocity next-velocity :acceleration next-acceleration)))
+
+  Massiv
+  (apply-force [this force] 
+    ; Newton's 2nd law: F = M * A
+    ; or A = F / M"
+    (let [f (PVector/div force (float mass))
+          next-acceleration (PVector/add acceleration f)]
+      (assoc this :acceleration next-acceleration)))
+
+  Movable
+  (check-edges [this]
+    (if (> (.-y location) (q/height)) 
+      (assoc this :location (PVector. (.-x location) (q/height)) :velocity (PVector/mult velocity (float -0.9)))
+      this))
+
+  Drawable
+  (draw [this]
+    (q/stroke 0)
+    (q/stroke-weight 2)
+    (q/fill color, 200)
+    (q/ellipse (.-x location) (.-y location) (* mass 16) (* mass 16))
+    this))
+
+;;;
+;;; Fluid
+;;;
+
+(defprotocol Permeable
+  (contains? [this mover] "check if mover is inside of the aetheral object")
+  (drag-force [this obj] "calcs the drag-force that affects the mover inside of the aetheral object"))
+
+(defrecord Fluid [id x y width height color drag-coefficient]
+  Permeable 
+  (contains? 
+    [this {mover-location :location}]
+    (let [mover-x (.-x mover-location)
+          mover-y (.-y mover-location)]
+      (if (and 
+            (> mover-x x) (< mover-x (+ x width)) 
+            (> mover-y y) (< mover-y (+ y height))) 
+        true
+        false)))                                                                  
+
+  (drag-force [{c :drag-coefficient} {velocity :velocity}] 
+    (let [speed (.mag velocity)
+          drag-magnitude (* c speed speed)
+          drag-force (.get velocity)]
+      (.mult drag-force (float -1)) ; Seiteneffekt
+      (.normalize drag-force) ; Seiteneffekt
+      (.mult drag-force (float drag-magnitude)) ;Seiteneffekt
+      drag-force))
+
+  Drawable
+  (draw [{:keys [x y width height color]}]
+    (q/no-stroke)
+    (q/fill color)
+    (q/rect x y width height)))
+
+;;;
+;;; Main
+;;;
+
 (def params 
-  {:size [800 200]
+  {:size [600 400]
    :background 255
    :frame-rate 30
-   :mover-count 5
-   :mover-r 48
-   :speed-x 0
-   :speed-y 0
-   :topspeed 5 
-   :acceleration 0.2})
+   :mover-count 3 
+   :initial-speed-x 0
+   :initial-speed-y 0
+   :initial-acceleration-x 0
+   :initial-acceleration-y 0
+   :fluid-color 211}) 
 
-(def movers
+(defn make-fluid []
+  (map->Fluid 
+    {:id "fluid1" 
+     :x 0 :y (* (second (params :size)) 0.75) :width (first (params :size)) :height (second (params :size))  
+     :color 127 :drag-coefficient 1.0}))
+
+(defn make-movers []
+  (map 
+    (fn [id]
+      (map->Mover 
+        {:id (str "mover" id)
+         :mass (inc (rand-int 3)) ; TODO            
+         :location (PVector. (rand-int (first (params :size))) (/ (rand-int (second (params :size))) 2)) 
+         :velocity (PVector. (params :initial-speed-x) (params :initial-speed-y))
+         :acceleration (PVector. (params :initial-acceleration-x) (params :initial-acceleration-y))
+         :color (params :fluid-color)}))
+    (range (params :mover-count))))
+
+(def view-model
   (atom
-    (map (fn [id]
-           {:id id
-            :location (PVector. (rand-int (first (params :size))) (rand-int (second (params :size)))) 
-            :velocity (PVector. (params :speed-x) (params :speed-y))})
-         (range (params :mover-count)))))
+    { :fluid  (make-fluid)
+      :movers (make-movers)}))
 
-(def scene-graph
-  (let [mover-node (mover/make-mover-node)]
-        (scene/->GraphNode
-          []
-          identity
-          mover/draw)]
-    mover-node))
+(defn apply-gravity [mover]
+  ; Gravity is scaled by mass here!
+  (let [gravity (PVector. 0 (* 0.1 (:mass mover)))]
+    (apply-force mover gravity)))
+
+(defn apply-drag-force [mover]
+  (let [fluid (@view-model :fluid)]
+    ; Is the Mover in the liquid ?
+    (if (contains? fluid mover)
+      (let [drag-force (drag-force fluid mover)]
+        (apply-force mover drag-force))
+      mover)))
+
+(defn movers-next-state [movers]
+  (map (comp next-state apply-drag-force apply-gravity) movers)) ; comp wendet von rechts nach links an
 
 (defn setup []
   (q/frame-rate (params :frame-rate))
-  (q/background (params :background))
   (q/smooth))
 
-(defn calc-acceleration 
-  "Compute acceleration-vector that points from source to target
-  set magnitude of acceleration in params"
-  [source-location target-location]
-  (let [ target-v (PVector/sub target-location source-location)
-         target-nv (do (.normalize target-v) target-v) ; Seiteneffekt !
-         ; Set magnitude of acceleration
-         acceleration (PVector/mult target-nv (float (params :acceleration)))]
-    acceleration))
-
-(defn calc-velocity
-  "Compute new velocity-vector by adding acceleration
-   and constrain with topspeed"
-  [velocity acceleration]
-  (let [a-velocity (PVector/add velocity acceleration)
-        l-velocity (do (.limit a-velocity (float (params :topspeed))) a-velocity)]
-    l-velocity)) 
-        
-(defn update-mover [mover target-location]
-  (let [ 
-        m-id (:id mover)
-        m-location (:location mover)
-        m-velocity (:velocity mover)
-        ; Location changes by velocity
-        next-location (PVector/add m-location m-velocity)
-        ; Velocity changes by acceleration
-        acceleration (calc-acceleration m-location target-location)
-        next-velocity (calc-velocity m-velocity acceleration)]
-    { :id m-id :location next-location :velocity next-velocity }))
-
-(defn update-movers [movers target-location] 
-  (map #(update-mover % target-location) movers))
+(defn mouse-pressed []
+  (swap! view-model #(update-in % [:movers] (constantly (make-movers)))))
 
 (defn draw []
   ; draw Background
@@ -85,22 +160,23 @@
   (q/fill 255) 
   (q/rect 0 0 (q/width) (q/height))
 
+  ; draw fluid
+  (draw (@view-model :fluid))
+
   ; draw movers
-  (q/stroke 0)
-  (q/stroke-weight 2)
-  (q/fill (q/random 100 200))
-  
-  (dorun (map #(scene/draw scene-graph (:location %)) @movers))
+  (dorun (map #(draw %) (@view-model :movers)))
 
   ; draw hint(s)
   (q/fill 0)
   (q/text "click mouse to reset" 10 30)
 
-  ; update movers to next state
-  (swap! movers #(update-movers % (PVector. (q/mouse-x) (q/mouse-y))))) 
+  ; update view-model to next state
+  (swap! view-model #(update-in % [:movers] movers-next-state)))
 
 (q/defsketch fluidresistance
   :title "Bodies experience gravity and fluid resistance"
   :setup setup
   :draw draw
+  :mouse-pressed mouse-pressed
   :size (params :size))
+
