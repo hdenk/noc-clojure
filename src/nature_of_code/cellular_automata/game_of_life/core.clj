79be051d404c3e628cc3010399122ebf6db7a287
hdenk <helmut.denk@gmx.de>
2012-12-20 11:11:55 +0100

    cellular-automata.game-of-life doseq can be nested

--- src/nature_of_code/cellular_automata/game_of_life/core.clj
+++ src/nature_of_code/cellular_automata/game_of_life/core.clj
@@ -40,11 +40,12 @@
 
 (def board (atom nil))
 
-(defn random-initial-cells []
-  (condp = (rand-int 3)
+(defn random-initial-cells [rows columns]
+  (condp = (rand-int 4)
     0 #{[1 0] [1 1] [1 2]} ; alternating-cross     
     1 #{[2 0] [2 1] [2 2] [1 2] [0 1]} ; glider
-    2 #{[2 0] [4 0] [1 1] [1 2] [1 3] [4 3] [1 4] [2 4] [3 4]})) ; light-spaceship
+    2 #{[2 0] [4 0] [1 1] [1 2] [1 3] [4 3] [1 4] [2 4] [3 4]} ; light-spaceship
+    3 (set (for [x (range rows), y (range columns)] [x y])))) ; all cells alive
 
 (defn setup-sketch []
   (q/frame-rate (params :frame-rate))
@@ -52,25 +53,25 @@
 
   (let [rows (/ (size-y) (params :cell-w))
         columns (/ (size-x) (params :cell-w))]
-    (swap! board (constantly (gen-board :rows rows :columns columns :w (params :cell-w) :cells (random-initial-cells))))))
+    (swap! board (constantly (gen-board :rows rows :columns columns :w (params :cell-w) :cells (random-initial-cells rows columns))))))
 
 (defn draw-sketch []
   (q/background 255)
   ; draw cells    
   (let [w (:w @board)]
-  (doseq [x (range (:rows @board))]
-    (doseq [y (range (:columns @board))]
+    (doseq [x (range (:rows @board)) 
+            y (range (:columns @board))]
       (if (contains? (:cells @board) [x y]) 
         (q/fill 0) 
         (q/fill 255))
       (q/stroke 0)
-      (q/rect (* x w) (* y w) w w)))) 
-   
+      (q/rect (* x w) (* y w) w w))) 
+
   ; next cells-state    
   (swap! board assoc :cells (step (:cells @board))))
 
 (defn mouse-pressed [] 
-  (swap! board assoc :cells (random-initial-cells)))
+  (swap! board assoc :cells (random-initial-cells (:rows @board) (:columns @board))))
 
 (q/defsketch conways-game-of-life 
   :title "implementation of conways game of life"
