1b4a4aff1e437a36f572125fffa93830f6c7d8d7
hdenk <helmut.denk@gmx.de>
2013-02-11 14:16:17 +0100

    update buildfile: remove dependencies for core.reducers and go back to clojure 1.4.0

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://natureofcode.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [ [org.clojure/clojure "1.5.0-beta1"]
-                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
+  :dependencies [ [org.clojure/clojure "1.4.0"]
                   [quil "1.6.0"] ])
