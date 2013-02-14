55db3e84f8377b389337d9d127b26afe3c613c9a
hdenk <helmut.denk@gmx.de>
2013-02-14 19:39:09 +0100

    refactor sample: forces.fluidresistance

--- src/nature_of_code/forces/fluidresistance/core.clj
+++ src/nature_of_code/forces/fluidresistance/core.clj
@@ -3,97 +3,18 @@
    Bodies experience gravity continuously
    Bodies experience fluid resistance when in water
    Based on the Nature of Code by Daniel Shiffman http://natureofcode.com"
-   (:require [quil.core :as q])
-   (:import [processing.core PVector]))
-
-;;;
-;;; Mover
-;;;
-
-(defprotocol Mobile
-  (move [this] "calc next state for the stateful object")
-  (check-edges [this] "check if the drawable object is out ouf bounds and react"))
-
-(defprotocol Massiv
-  (apply-force [this force] "apply force to the masive object"))
-
-(defprotocol Drawable
-  (draw [this] "draw the drawable object to an output-device"))
-
-(defrecord Mover [id mass location velocity acceleration color]
-  Mobile  
-  (move [this]
-    (let [next-location (PVector/add location velocity)
-          next-velocity (PVector/add velocity acceleration)
-          next-acceleration (PVector/mult acceleration (float 0))]
-      (assoc this :location next-location :velocity next-velocity :acceleration next-acceleration)))
-
-  (check-edges [this]
-    (if (> (.-y location) (q/height)) 
-      (assoc this :location (PVector. (.-x location) (q/height)) :velocity (PVector/mult velocity (float -0.9)))
-      this))
-
-  Massiv
-  (apply-force [this force] 
-    ; Newton's 2nd law: F = M * A
-    ; or A = F / M"
-    (let [f (PVector/div force (float mass))
-          next-acceleration (PVector/add acceleration f)]
-      (assoc this :acceleration next-acceleration)))
-
-  Drawable
-  (draw [this]
-    (q/stroke 0)
-    (q/stroke-weight 2)
-    (q/fill color, 200)
-    (q/ellipse (.-x location) (.-y location) (* mass 16) (* mass 16))
-    this))
-
-;;;
-;;; Fluid
-;;;
-
-(defprotocol Permeable
-  (contains-mover? [this mover] "check if mover is inside of the aetheral object")
-  (drag-force [this obj] "calcs the drag-force that affects the mover inside of the aetheral object"))
-
-(defrecord Fluid [id x y width height color drag-coefficient]
-  Permeable 
-  (contains-mover? 
-    [this {mover-location :location}]
-    (let [mover-x (.-x mover-location)
-          mover-y (.-y mover-location)]
-      (if (and 
-            (> mover-x x) (< mover-x (+ x width)) 
-            (> mover-y y) (< mover-y (+ y height))) 
-        true
-        false)))                                                                  
-
-  (drag-force [{c :drag-coefficient} {velocity :velocity}] 
-    (let [speed (.mag velocity)
-          drag-magnitude (* c speed speed)
-          drag-force (.get velocity)]
-      (.mult drag-force (float -1)) ; Seiteneffekt
-      (.normalize drag-force) ; Seiteneffekt
-      (.mult drag-force (float drag-magnitude)) ;Seiteneffekt
-      drag-force))
-
-  Drawable
-  (draw [{:keys [x y width height color]}]
-    (q/no-stroke)
-    (q/fill color)
-    (q/rect x y width height)))
-
-;;;
-;;; Main
-;;;
+  (:require [quil.core :as qc])
+  (:use [nature-of-code.math.vector :as mv]))
 
 (def params 
-  {:size [600 400]
+  {:size-x 600 
+   :size-y 400
    :background 255
    :frame-rate 30
    :mover-count 5 
    :mass-classes 3 
+   :r-factor 16
+   :re-bouncing-factor -0.6
    :initial-speed-x 0
    :initial-speed-y 0
    :initial-acceleration-x 0
@@ -102,11 +23,98 @@
    :mover-color 127
    :fluid-color 211}) 
 
+;;;
+;;; Fluid
+;;;
+
+(defrecord Fluid [id x y width height color drag-coefficient])
+  
+(defn contains-mover? 
+  "takes a fluid and a mover and returns true, if the mover is inside the fluid" 
+  [{:keys [x y width height]} {mover-location :location}]
+  (let [[mover-x mover-y] mover-location]
+    (if (and 
+          (> mover-x x) (< mover-x (+ x width)) 
+          (> mover-y y) (< mover-y (+ y height))) 
+      true
+      false)))                                                                  
+
+(defn drag-force 
+  "takes a fluid and a mover and returns drag-force" 
+  [{:keys [drag-coefficient] :as fluid} {:keys [velocity] :as mover}] 
+  (if (contains-mover? fluid mover)
+	  (let [speed (mv/magnitude velocity)
+	        drag-magnitude (* drag-coefficient speed speed)
+	        drag-force (mv/multiply velocity (float -1))] 
+	    (-> (mv/normalize drag-force)
+	        (mv/multiply (float drag-magnitude))))
+    [0 0]))
+
+(defn draw-fluid
+  [{:keys [x y width height color] :as fluid}]
+    (qc/no-stroke)
+    (qc/fill color)
+    (qc/rect x y width height)
+    fluid)
+
 (defn make-fluid []
   (map->Fluid 
     {:id "fluid1" 
-     :x 0 :y (* (second (params :size)) 0.75) :width (first (params :size)) :height (second (params :size))  
+     :x 0 :y (* (params :size-y) 0.75) :width (params :size-x) :height (params :size-y)  
      :color (params :fluid-color) :drag-coefficient (params :drag-coefficient)}))
+
+;;;
+;;; Mover
+;;;
+
+(defrecord Mover [id mass location velocity acceleration color])
+
+(defn apply-force [{:keys [acceleration mass] :as mover} force] 
+  "takes a mover and a force, applies the force and returns a mover with changed acceleration"
+  ; Newton's 2nd law: F = M * A
+  ; or A = F / M"
+	(let [f (mv/divide force (float mass))
+	      next-acceleration (mv/add acceleration f)]
+	  (assoc mover :acceleration next-acceleration)))
+ 
+(defn apply-gravity [mover]
+  ; Gravity is scaled by mass here!
+  (let [gravity [0 (* 0.1 (:mass mover))]]
+    (apply-force mover gravity)))
+
+(defn update-motion-state 
+  "takes a mover and force returns a mover with updated motion-state"
+   [{:keys [location velocity acceleration] :as mover}]
+    (let [next-location (mv/add location velocity)
+          next-velocity (mv/add velocity acceleration)
+          next-acceleration (mv/multiply acceleration (float 0))]
+      (assoc mover :location next-location :velocity next-velocity :acceleration next-acceleration)))
+
+(defn check-edges [{:keys [location velocity mass] :as mover}]
+  (let [[x y] location
+        rh (/ (* (params :r-factor) mass) 2)
+        y-max (- (qc/height) rh)]
+    (if (> y y-max) 
+      (assoc mover :location [x y-max] :velocity (mv/multiply velocity (float (params :re-bouncing-factor))))
+      mover)))
+
+(defn update-mover
+   "takes a mover and force returns a mover with updated motion-state and applied force"
+   [mover fluid]
+    (let [drag-force (drag-force fluid mover)]
+      (-> (apply-gravity mover)
+          (apply-force drag-force) 
+          (update-motion-state)
+          (check-edges))))
+
+(defn draw-mover 
+  [{:keys [location mass color] :as mover}]
+  (qc/stroke 0)
+  (qc/stroke-weight 2)
+  (qc/fill color, 200)
+  (let [[x y] location]
+    (qc/ellipse x y (* mass (params :r-factor)) (* mass (params :r-factor))))
+  mover)
 
 (defn make-movers []
   (map 
@@ -114,80 +122,70 @@
       (map->Mover 
         {:id (str "mover" id)
          :mass (inc (rand-int (params :mass-classes)))              
-         :location (PVector. (rand-int (first (params :size))) (/ (rand-int (second (params :size))) 2)) 
-         :velocity (PVector. (params :initial-speed-x) (params :initial-speed-y))
-         :acceleration (PVector. (params :initial-acceleration-x) (params :initial-acceleration-y))
+         :location [(rand-int (params :size-x)) (/ (rand-int (params :size-y)) 2)] 
+         :velocity [(params :initial-speed-x) (params :initial-speed-y)]
+         :acceleration [(params :initial-acceleration-x) (params :initial-acceleration-y)]
          :color (params :mover-color)}))
     (range (params :mover-count))))
 
-(def view-model
+;;;
+;;; Main
+;;;
+
+(def sketch-model
   (atom
-    { :fluid  (make-fluid)
-      :movers (make-movers)}))
+    { :fluid nil 
+      :movers nil}))
 
-(defn apply-gravity [mover]
-  ; Gravity is scaled by mass here!
-  (let [gravity (PVector. 0 (* 0.1 (:mass mover)))]
-    (apply-force mover gravity)))
+(defn init-sketch-model [m-atom] 
+  (swap! 
+    m-atom 
+    (fn [m] 
+      (-> (assoc-in m [:fluid] (make-fluid))
+          (assoc-in [:movers] (make-movers))))))
 
-(defn apply-drag-force [mover]
-  (let [fluid (@view-model :fluid)]
-    ; Is the Mover in the liquid ?
-    (if (contains-mover? fluid mover)
-      (let [drag-force (drag-force fluid mover)]
-        (apply-force mover drag-force))
-      mover)))
+(defn update-movers [fluid movers]
+  (map #(update-mover % fluid) movers))
 
-(defn movers-next-state [movers]
-  (map 
-    #(->
+(defn setup-sketch []
+  (qc/frame-rate (params :frame-rate))
+  (qc/smooth) ; anti aliasing on
+  (init-sketch-model sketch-model))
+
+(defn draw-sketch []
+  ; draw Background
+  (qc/background (params :background))
+
+  ; draw fluid
+  (draw-fluid (@sketch-model :fluid))
+
+  ; draw movers
+  (dorun (map #(draw-mover %) (@sketch-model :movers)))
+
+  ; draw hint(s)
+  (qc/fill 0)
+  (qc/text "click mouse to reset" 10 30)
+
+  ; update sketch-model to next state
+  (swap! 
+    sketch-model 
+    #(update-in 
        % 
-       (apply-gravity) 
-       (apply-drag-force) 
-       (move)) 
-    movers)) 
-
-(defn setup []
-  (q/frame-rate (params :frame-rate))
-  (q/smooth))
+       [:movers] 
+       (partial update-movers (@sketch-model :fluid)))))
 
 (defn mouse-pressed []
   (swap! 
-    view-model 
+    sketch-model 
     #(update-in 
        % 
        [:movers] 
        (constantly (make-movers)))))
 
-(defn draw []
-  ; draw Background
-  (q/no-stroke)
-  (q/fill (params :background)) 
-  (q/rect 0 0 (q/width) (q/height))
-
-  ; draw fluid
-  (draw (@view-model :fluid))
-
-  ; draw movers
-  (dorun (map #(draw %) (@view-model :movers)))
-
-  ; draw hint(s)
-  (q/fill 0)
-  (q/text "click mouse to reset" 10 30)
-
-  ; update view-model to next state
-  (swap! 
-    view-model 
-    #(update-in 
-       % 
-       [:movers] 
-       movers-next-state)))
-
-(defn run []
-	(q/defsketch fluidresistance
+(defn run-sketch []
+	(qc/defsketch fluidresistance
 	  :title "Bodies experience gravity and fluid resistance"
-	  :setup setup
-	  :draw draw
+	  :setup setup-sketch
+	  :draw draw-sketch
 	  :mouse-pressed mouse-pressed
-	  :size (params :size)))
-
+    :size [(params :size-x) (params :size-y)]))
